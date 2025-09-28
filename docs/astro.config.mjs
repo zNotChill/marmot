4a67472a165544461ece23747efd9f09ddb5cda5
zNotChill <znotchill@znci.dev>
2025-09-28 01:12:16 +0100

    fix: docs workflow again x2

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://marmot.znotchill.me",
-	base: "/docs/",
+	base: "/",
 	integrations: [
 		starlight({
 			customCss: [
